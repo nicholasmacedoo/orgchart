8a6026a0f0c9d18973d43e407eb8f6e24fadca77
Nicholas Macedo <33987691+nicholasmacedoo@users.noreply.github.com>
2022-01-25 12:30:30 -0300

    fix: unnecessary import deleted

--- src/Dashboard.tsx
+++ src/Dashboard.tsx
@@ -1,27 +1,17 @@
 import 'react-orgchart/index.css';
 import { Header } from './components/Header';
-import { MyTree } from './components/MyTree';
-import { useNavigate } from 'react-router-dom';
+import { Tree } from './components/Tree';
 
 import styles from './home.module.scss';
 import data from './data.json';
-import { Tree } from './components/Tree';
 
 function Dashboard() {
 
-    const navigate = useNavigate();
 
     return (
         <div>
         <Header />
         <div className={styles.container}>
-            {/* <MyTree
-            onClick={(id: number) => {
-                navigate(`/view/${id}`);
-            }} 
-            //   onClickNode={(id: number) => console.log(id)}
-            nodes={data}
-            /> */}
             <Tree 
                 nodes={data} 
                 onClick={(id: number) => console.log({ id })}    
